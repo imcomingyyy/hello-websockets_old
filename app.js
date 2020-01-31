f5189515390718de6e99d02943f822f543292d2a
YYY <1045991635@qq.com>
2020-01-30 19:11:11 -0600

    created a websocket server resolved issue/5

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // npm install --save express http url ws
 const express =require ('express');
 const http= require('http');
-
+const Websocket =require('ws');
 const app =express();
 app.use(express.static('public'));
 
 const server = http.createServer(app);
+const wss = new Websocket.Server({server:server});
+wss.on('connection',function(ws,request){
+    console.log('Connected!');
+});
 
 server.listen(8080,function(){
     console.log('App listening on port 8080');

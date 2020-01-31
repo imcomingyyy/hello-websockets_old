15b40179333415eb9636d54d1f291ffff4a7844d
YYY <1045991635@qq.com>
2020-01-30 19:45:47 -0600

    new updated

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ const server = http.createServer(app);
 const wss = new Websocket.Server({server:server});
 wss.on('connection',function(ws,request){
     console.log('Connected!');
+    ws.on('message', function(message){
+        wss.clients.forEach(function(clients){
+            if (clients.readyState === Websocket.OPEN){
+                clients.send(message);
+            }
+        });
+    });
 });
 
 server.listen(8080,function(){
